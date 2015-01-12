4f1787e401f29e7b539e22773c29addc7103d91f
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2015-01-12 02:58:35 +0100

    Bumo to 0.2.1 with fixed header documentation

--- NGRValidator/NGRValidator/NGRValidator.h
+++ NGRValidator/NGRValidator/NGRValidator.h
@@ -35,26 +35,47 @@ extern NGRPropertyValidator * NGRValidate(NSString *property);
 + (NSError *)validateValue:(NSObject *)value named:(NSString *)name usingRules:(void (^)(NGRPropertyValidator *validator))rules;
 
 /**
- *  Validates given model given selected rules. Returns YES when validation succeeded, NO otherwise.
+ *  Validates model with given rules. Returns YES when validation succeeded, NO otherwise.
  *
- *  @param model Model within properties are validated
- *  @param error Validation error with decription, nil when validation will pass
- *  @param rules An array of validation rules. Every property within a model is validated separately
+ *  @param model Model within properties are validated.
+ *  @param error Validation error with decription, nil when validation will pass.
+ *  @param rules An array of validation rules. Every property within a model is validated separately.
+ *
  *  @return result of validation. YES if validation will pass, NO otherwise.
  */
 + (BOOL)validateModel:(NSObject *)model error:(NSError **)error usingRules:(NSArray *(^)())rules;
 
+/**
+ *  Validates model with given rules and scenario. Returns YES when validation succeeded, NO otherwise.
+ *
+ *  @param model Model within properties are validated.
+ *  @param error Validation error with decription, nil when validation will pass.
+ *  @param scenario The scenario specifying which properties of model should be validated.
+ *  @param rules An array of validation rules. Every property within a model is validated separately.
+ *
+ *  @return result of validation. YES if validation will pass, NO otherwise.
+ */
 + (BOOL)validateModel:(NSObject *)model error:(NSError **)error scenario:(NSString *)scenario usingRules:(NSArray *(^)())rules;
 
 /**
- *  Validates given model with given rules. Returns empty array when validation succeeded. If not, array will contains errors.
+ *  Validates model with given rules. Returns empty array when validation succeeded. If not, array will contains errors.
  *
- *  @param model Model within properties are validated
- *  @param rules An array of validation rules. Every property within a model is validated separately
+ *  @param model Model within properties are validated.
+ *  @param rules An array of validation rules. Every property within a model is validated separately.
+ *
  *  @return an array of errors. Nil if validation will pass, otherwise will contain NSError objects.
  */
 + (NSArray *)validateModel:(NSObject *)model usingRules:(NSArray *(^)())rules;
 
+/**
+ *  Validates model with given rules and scenario. Returns empty array when validation succeeded. If not, array will contains errors.
+ *
+ *  @param model Model within properties are validated.
+ *  @param scenario The scenario specifying which properties of model should be validated.
+ *  @param rules An array of validation rules. Every property within a model is validated separately.
+ *
+ *  @return an array of errors. Nil if validation will pass, otherwise will contain NSError objects.
+ */
 + (NSArray *)validateModel:(NSObject *)model scenario:(NSString *)scenario usingRules:(NSArray *(^)())rules;
 
 @end
