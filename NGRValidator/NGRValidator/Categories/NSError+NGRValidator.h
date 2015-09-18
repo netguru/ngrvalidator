6caf179da421b476c2b7586da8ac58e8272c44d3
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-09-18 14:15:31 +0200

    reveal error domain

--- NGRValidator/NGRValidator/Categories/NSError+NGRValidator.h
+++ NGRValidator/NGRValidator/Categories/NSError+NGRValidator.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+extern NSString * const NGRValidatorDomain;
+
 @interface NSError (NGRValidator)
 
 + (instancetype)ngr_errorWithDescription:(NSString *)description;
